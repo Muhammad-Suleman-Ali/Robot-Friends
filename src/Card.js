8905bf1a863ab721d170d56734f98fa8a5b1065a
Muhammad-Suleman-Ali <70519146+Muhammad-Suleman-Ali@users.noreply.github.com>
2020-09-28 17:32:01 +0500

    Card Component created

---
+++ src/Card.js
@@ -0,0 +1,20 @@
+import React ,{Component} from 'react';
+import ReactDOM from 'react-dom';
+
+
+ const Card =()=>{
+    
+    
+    
+    return ( <>
+        <div> 
+        <img src="https://robohash.org/test?2000*2000" alt="Robots"/>
+        <div>
+            <h2> Name</h2>
+            <p> email</p>
+        </div>
+        </div>
+
+    </>) 
+}
+export default Card;
