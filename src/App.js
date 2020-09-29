45b10b57b6c10f78d8be710d3f30bb8274ef4013
Muhammad-Suleman-Ali <70519146+Muhammad-Suleman-Ali@users.noreply.github.com>
2020-09-30 00:37:40 +0500

    Scroll Added

--- src/App.js
+++ src/App.js
@@ -3,27 +3,36 @@ import CardArray from './CardArray.js';
 import Searchfield from './Searchfield.js';
 import 'tachyons'
 import {Content} from './Content';
+import Scroll from './Scroll';
 
 
 export default class App extends Component{
     state={
-        content: Content,
+        content:[],
         searchfield:''
     }
+componentDidMount(){
+    fetch('https://jsonplaceholder.typicode.com/users').then((Response)=>{
+        return Response.json();
+    })
+    .then(users=>{
+        this.setState({ content:users })
+    })
+}
     onChange=(e)=>{
        this.setState({searchfield:e.target.value}) 
     }
  render(){
-    const filterarry = Content.filter((num)=>{
-        return num.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
+    const filterarry = this.state.content.filter((num)=>{
+    return num.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
              })
-         return (< >
+         return (<>
         <div className='tc'>
-            <div className='tc bg-light-blue ma2 pa3 b--green '>    
-                <h1>RoboFriends</h1>
+                <h1 className='f1'>RoboFriends</h1>
                 <Searchfield onchange={this.onChange}/> 
-            </div>
+              <Scroll>
                 <CardArray filterarry={filterarry}/>
-           </div>
+              </Scroll>   
+              </div>
         </>) 
 }}
